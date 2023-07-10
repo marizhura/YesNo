8f255b45d46d1c30f3c13624a9296257666179ea
Марина <77566874+marizhura@users.noreply.github.com>
2023-07-10 17:14:59 +0300

    Initial commit

---
+++ YesNo/YesNo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YesNo
+//
+//  Created by Марина Журавлева on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
